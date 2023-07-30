c367fb8a921e1d509a6920913f3ae4108262eb0a
sebbyapudo <sebbyapudo@gmail.com>
2023-07-31 02:16:19 +0300

    Implemented the nav toggle functionality on smaller screens

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,26 @@
 // Skiongozi team building
 // Global variable declaration
 
+// Set currrent date
+const date = document.getElementById('date');
+const currrentDate = new Date().getFullYear();
+date.innerHTML = currrentDate;
+
+// Close Links
+const linksContainer = document.querySelector('.links-container')
+const links = document.querySelector('.links')
+const navToggle = document.querySelector('.nav-toggle')
+
+navToggle.addEventListener('click', () => {
+  // linksContainer.classList.toggle('show-links')
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+
+  if(containerHeight === 0){
+    linksContainer.style.height = `${linksHeight}px`;
+  }
+  else{
+    linksContainer.style.height = 0;
+  }
+})
 
