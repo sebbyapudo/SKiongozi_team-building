6b724ee7a83b83a2384b216d8b017c7f3e65fd14
sebbyapudo <sebbyapudo@gmail.com>
2023-07-31 20:46:12 +0300

    Fixed a bug that prevented scroll events and click events on the navToggle

--- src/js/app.js
+++ src/js/app.js
@@ -47,5 +47,33 @@ window.addEventListener('scroll', () => {
 })
 
 // Smooth scroll
+// select link
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach( (link) => {
+  link.addEventListener("click", (e) => {
+    // prevent default
+    e.preventDefault()
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    // calculate heights 
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains('fixed-nav');
+    let position = element.offsetTop - navHeight;
+
+    if(!fixedNav){
+      position = position - navHeight;
+    }
+
+    window.scrollTo({ 
+      left:0,
+      top: position,
+    });  
+    // close
+    linksContainer.classList.remove('show-links')
+  });
+});
 
 
