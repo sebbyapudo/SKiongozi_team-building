27989f4f6cf7fa514f9fa36d2099cb2ecd97e46d
sebbyapudo <sebbyapudo@gmail.com>
2023-07-31 20:55:02 +0300

    Implemented dynamic smooth scroll behaviour on the page

--- src/js/app.js
+++ src/js/app.js
@@ -66,6 +66,9 @@ scrollLinks.forEach( (link) => {
     if(!fixedNav){
       position = position - navHeight;
     }
+    if(navHeight > 82){
+      position = position + containerHeight;
+    }
 
     window.scrollTo({ 
       left:0,
