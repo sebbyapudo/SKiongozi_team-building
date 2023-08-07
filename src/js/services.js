64c7295ea3aca85e544390d9411341bdf94b65aa
sebbyapudo <sebbyapudo@gmail.com>
2023-08-08 02:13:15 +0300

    Updated the services array

---
+++ src/js/services.js
@@ -0,0 +1,36 @@
+const services = [{
+  id: 1,
+  title: 'corporate team building',
+  img: './src/images/corporate_team-building.jpg',
+  desc: 'Enhance your company\'s synergy with our top-tier corporate team building services. Engage in dynamic activities that foster collaboration, boost morale, and elevate productivity.'
+},
+{
+  id: 1,
+  title: 'corporate team building',
+  img: './src/images/corporate_team-building.jpg',
+  desc: 'Enhance your company\'s synergy with our top-tier corporate team building services. Engage in dynamic activities that foster collaboration, boost morale, and elevate productivity.'
+},
+{
+  id: 1,
+  title: 'corporate team building',
+  img: './src/images/corporate_team-building.jpg',
+  desc: 'Enhance your company\'s synergy with our top-tier corporate team building services. Engage in dynamic activities that foster collaboration, boost morale, and elevate productivity.'  
+},
+{
+  id: 1,
+  title: 'corporate team building',
+  img: './src/images/corporate_team-building.jpg',
+  desc: 'Enhance your company\'s synergy with our top-tier corporate team building services. Engage in dynamic activities that foster collaboration, boost morale, and elevate productivity.'
+},
+{
+  id: 1,
+  title: 'corporate team building',
+  img: './src/images/corporate_team-building.jpg',
+  desc: 'Enhance your company\'s synergy with our top-tier corporate team building services. Engage in dynamic activities that foster collaboration, boost morale, and elevate productivity.'
+},
+{
+  id: 1,
+  title: 'corporate team building',
+  img: './src/images/corporate_team-building.jpg',
+  desc: 'Enhance your company\'s synergy with our top-tier corporate team building services. Engage in dynamic activities that foster collaboration, boost morale, and elevate productivity.'
+}]
