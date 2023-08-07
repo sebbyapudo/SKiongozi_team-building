5d31da259dfc70750cdd88b9039f6eb9a35bffa4
sebbyapudo <sebbyapudo@gmail.com>
2023-08-08 01:37:42 +0300

    Set up the blog array of objects

---
+++ src/js/blog.js
@@ -0,0 +1,18 @@
+const blog = [{
+  id: 1,
+  title: 'Why team building',
+  img: './src/images/blog-4.png',
+  desc: 'Discover the Power of Team Building: Unleash Success Together. Join the Journey Now!',
+},
+{
+  id: 2,
+  title: 'Giving back to society',
+  img: './src/images/blog-2.jpg',
+  desc: 'Building Bonds, Building Hope: How Team Building Can Create Lasting Impact for Society\'s Betterment.',
+},
+{
+  id: 3,
+  title: 'Optimum team performance',
+  img: './src/images/blog-3.jpg',
+  desc: 'Peak Performance Unleashed: Uncover the Secrets to Optimum Team Success.',
+}]
